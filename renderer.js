ba3f3bc0e9684d68fecf2466bb01f26372db8f81
VihangaWicks <vihangaw@tstpl.com>
2026-06-30 21:36:39 +0530

    live url

--- renderer.js
+++ renderer.js
@@ -24,15 +24,15 @@ let currentExamData = {
     qr: null
 };
 
-// const CONFIG = {
-//     BASE_API_URL: 'https://sms.metropolitancollegeedu.com/lms-mc',
-//     EXAM_BASE_URL: 'https://exams.metropolitancollege.lk/lms-exam',
-//     BASE_LMS_URL: 'https://www.metropolitancollegeedu.com/lms-mc',
-//     KURENTO: 'wss://localhost:8443/kurento-group-call/groupcall',
-//     BASE_LANDING: './landing.html',
-//     PROFILE: 'live',
-//     GEN_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWTEUiLCJuYW1lIjoiTE1TLU1DIiwiaWF0IjoxNjkxMzA2MDEwLCJhdXRob3IiOiJ2aWhhbmdhd2lja3MiLCJleHAiOjE5OTEzMDYwMTAsImlzcyI6Im1jOnZ5dzpqTWlGaWV6cjMxMyIsIm5iZiI6MTY5MTIwNTAwMH0.EAPlpsX1ZuoK5R_u4818-d4zJAIeXgXUKGqHu2x7SQM'
-// };
+const CONFIG = {
+    BASE_API_URL: 'https://sms.metropolitancollegeedu.com/lms-mc',
+    EXAM_BASE_URL: 'https://exams.metropolitancollege.lk/lms-exam',
+    BASE_LMS_URL: 'https://www.metropolitancollegeedu.com/lms-mc',
+    KURENTO: 'wss://localhost:8443/kurento-group-call/groupcall',
+    BASE_LANDING: './landing.html',
+    PROFILE: 'live',
+    GEN_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWTEUiLCJuYW1lIjoiTE1TLU1DIiwiaWF0IjoxNjkxMzA2MDEwLCJhdXRob3IiOiJ2aWhhbmdhd2lja3MiLCJleHAiOjE5OTEzMDYwMTAsImlzcyI6Im1jOnZ5dzpqTWlGaWV6cjMxMyIsIm5iZiI6MTY5MTIwNTAwMH0.EAPlpsX1ZuoK5R_u4818-d4zJAIeXgXUKGqHu2x7SQM'
+};
 // const CONFIG = {
 //     BASE_API_URL: 'https://mcp.metropolitancollegeedu.com/lms-mc',
 //     EXAM_BASE_URL: 'https://ems.metropolitancollegeedu.com/lms-exam',
@@ -42,15 +42,15 @@ let currentExamData = {
 //     PROFILE: 'testing',
 //     GEN_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWTEUiLCJuYW1lIjoiTE1TLU1DIiwiaWF0IjoxNjkxMzA2MDEwLCJhdXRob3IiOiJ2aWhhbmdhd2lja3MiLCJleHAiOjE5OTEzMDYwMTAsImlzcyI6Im1jOnZ5dzpqTWlGaWV6cjMxMyIsIm5iZiI6MTY5MTIwNTAwMH0.EAPlpsX1ZuoK5R_u4818-d4zJAIeXgXUKGqHu2x7SQM'
 // };
-const CONFIG = {
-    BASE_API_URL: 'http://localhost:8383/api/v1',
-    EXAM_BASE_URL: 'http://localhost:8384/api/v1',
-    BASE_LMS_URL: 'http://localhost:3001/testing',
-    KURENTO: 'wss://localhost:8443/kurento-group-call/groupcall',
-    BASE_LANDING: './landing.html',
-    PROFILE: 'local',
-    GEN_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWTEUiLCJuYW1lIjoiTE1TLU1DIiwiaWF0IjoxNjkxMzA2MDEwLCJhdXRob3IiOiJ2aWhhbmdhd2lja3MiLCJleHAiOjE5OTEzMDYwMTAsImlzcyI6Im1jOnZ5dzpqTWlGaWV6cjMxMyIsIm5iZiI6MTY5MTIwNTAwMH0.EAPlpsX1ZuoK5R_u4818-d4zJAIeXgXUKGqHu2x7SQM'
-};
+// const CONFIG = {
+//     BASE_API_URL: 'http://localhost:8383/api/v1',
+//     EXAM_BASE_URL: 'http://localhost:8384/api/v1',
+//     BASE_LMS_URL: 'http://localhost:3001/testing',
+//     KURENTO: 'wss://localhost:8443/kurento-group-call/groupcall',
+//     BASE_LANDING: './landing.html',
+//     PROFILE: 'local',
+//     GEN_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWTEUiLCJuYW1lIjoiTE1TLU1DIiwiaWF0IjoxNjkxMzA2MDEwLCJhdXRob3IiOiJ2aWhhbmdhd2lja3MiLCJleHAiOjE5OTEzMDYwMTAsImlzcyI6Im1jOnZ5dzpqTWlGaWV6cjMxMyIsIm5iZiI6MTY5MTIwNTAwMH0.EAPlpsX1ZuoK5R_u4818-d4zJAIeXgXUKGqHu2x7SQM'
+// };
 
 // LiveKit Configuration
 const LIVEKIT_CONFIG = {
